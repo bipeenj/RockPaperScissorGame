db62f6c130c5f7952581c567b55424fe76b25bf3
bipeenj <33595989+bipeenj@users.noreply.github.com>
2020-05-10 20:28:13 +0530

    changes in randomstratergy

--- RockPaperScissorGame/RandomNumberStratergy.cpp
+++ RockPaperScissorGame/RandomNumberStratergy.cpp
@@ -1,6 +1,12 @@
 #include "pch.h"
 #include "RandomNumberStratergy.h"
-
+#include <math.h>
+#include<time.h>
+#include <iostream>
+#include "Rock.h"
+#include "Paper.h"
+#include "VisitorBase.h"
+#include"Scissor.h"
 
 RandomNumberStratergy::RandomNumberStratergy()
 {
@@ -13,5 +19,33 @@ RandomNumberStratergy::~RandomNumberStratergy()
 
 GamePropObject * RandomNumberStratergy::getGameObject(VisitorBase* visitorIn)
 {
-	return nullptr;
+	auto rock = new Rock;
+	int rockint = visitorIn->Visit(rock);
+	auto paper = new Paper;
+	int paperint = visitorIn->Visit(paper);
+	auto scissor = new Scissor;
+	auto scissorint = visitorIn->Visit(scissor);
+	srand(time(0));
+	int num = rand();
+	GamePropObject* retVal = nullptr;
+	if (rockint == num)
+	{
+		retVal = rock;
+		delete paper;
+		delete scissor;
+	}
+	else if (paperint == num)
+	{
+		retVal = paper;
+		delete rock;
+		delete scissor;
+	}
+	else if (scissorint == num)
+	{
+		retVal = scissor;
+		delete paper;
+		delete rock;
+	}
+
+	return retVal;
 }
