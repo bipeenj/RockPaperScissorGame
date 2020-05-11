a71d1094597d7c5e4f3d7f240552e386e28c68a9
bipeenj <33595989+bipeenj@users.noreply.github.com>
2020-05-11 08:18:01 +0530

    final UI changes........

--- RockPaperScissorGame/RockPaperScissorGame.cpp
+++ RockPaperScissorGame/RockPaperScissorGame.cpp
@@ -14,46 +14,58 @@
 int main()
 {
 	GameObjectPrecedenceVisitor logicVisitor;
-    std::cout << "Welcome to game of Rock paper Scissor !\n Enter your name: "; 
-	std::string name;
-	std::cin >> name;
-	User newUser(name);
-	newUser.getGameObject(&logicVisitor);
-	Computer opponent;
-	opponent.getGameObject(&logicVisitor);
-	std::list<Player*> players = { &newUser,&opponent };
-	std::list<Player*> Winners;
-	
-	int gameTypeCount = logicVisitor.TotalGameObjectTypes();
-	int counter = 0;
-	for (auto it = players.begin(); it != players.end(); ++it)
+	char input = 'Y';
+	while (input == 'Y' || input == 'y')
 	{
-		auto obj = *it;
-		int counter2 = 0;
-		for (auto itNext = players.begin()  ; itNext != players.end(); ++itNext)
+		std::cout << "Welcome to game of Rock paper Scissor !\n Enter your name: ";
+		std::string name;
+		std::cin >> name;
+		User newUser(name);
+		newUser.getGameObject(&logicVisitor);
+		Computer opponent;
+		opponent.getGameObject(&logicVisitor);
+		std::list<Player*> players = { &newUser,&opponent };
+		std::list<Player*> Winners;
+
+		int gameTypeCount = logicVisitor.TotalGameObjectTypes();
+		int counter = 0;
+		for (auto it = players.begin(); it != players.end(); ++it)
 		{
-			if (counter2 <= counter)
+			auto obj = *it;
+			int counter2 = 0;
+			for (auto itNext = players.begin(); itNext != players.end(); ++itNext)
 			{
-				counter2++;
-				continue;
-			}
-			counter2++;
-			auto obj2 = *itNext;
-			int firstNo  = obj->getProp()->GetTypeNumber(&logicVisitor);
-			int secondNo = obj2->getProp()->GetTypeNumber(&logicVisitor);
-			auto resultNo = (gameTypeCount + firstNo - secondNo) % gameTypeCount;
-			if (resultNo != 0)
-			{
-				if ((resultNo % 2) > 0)
+				if (counter2 <= counter)
 				{
-					Winners.push_back(obj);
+					counter2++;
+					continue;
 				}
-				else
-					Winners.push_back(obj2);
+				counter2++;
+				auto obj2 = *itNext;
+				int firstNo = obj->getProp()->GetTypeNumber(&logicVisitor);
+				int secondNo = obj2->getProp()->GetTypeNumber(&logicVisitor);
+				auto resultNo = (gameTypeCount + firstNo - secondNo) % gameTypeCount;
+				if (resultNo != 0)
+				{
+					if ((resultNo % 2) > 0)
+					{
+						Winners.push_back(obj);
+					}
+					else
+						Winners.push_back(obj2);
+				}
+
 			}
-			
+			counter++;
 		}
-		counter++;
+		Winners.unique();
+		if (Winners.size() == 1)
+		{
+			auto name = Winners.front()->getName();
+			std::string winnermsg = "Winner is : " + name + "\n Do you want to play more? (Y/N)";
+			std::cout << winnermsg;
+		}
+		
+		std::cin >> input;
 	}
-	Winners.unique();
 }
